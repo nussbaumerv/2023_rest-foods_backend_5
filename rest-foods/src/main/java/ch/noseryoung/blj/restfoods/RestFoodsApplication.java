1eb5ac05c4385cd2432fd8c3d8fca1e56690570a
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2023-06-16 14:53:01 +0200

    initilnal commit with project

---
+++ rest-foods/src/main/java/ch/noseryoung/blj/restfoods/RestFoodsApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.blj.restfoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFoodsApplication.class, args);
+	}
+
+}
---
+++ rest-foods/src/test/java/ch/noseryoung/blj/restfoods/RestFoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.blj.restfoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
