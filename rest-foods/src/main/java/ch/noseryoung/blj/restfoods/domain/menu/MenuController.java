2ddb5e3329f9789b5070bf9ca5f0275f550b2d86
Loris Di Leo <loris.dileo@noseryoung.com>
2023-06-23 11:23:55 +0200

    Origin hinzugefügt

--- rest-foods/src/main/java/ch/noseryoung/blj/restfoods/domain/menu/MenuController.java
+++ rest-foods/src/main/java/ch/noseryoung/blj/restfoods/domain/menu/MenuController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @Log4j2
 @RestController
 @RequestMapping("/rest-foods/v1/menu")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MenuController {
     private final MenuService service;
 
