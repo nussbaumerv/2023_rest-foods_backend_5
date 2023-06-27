57934928f444d6d861c17cc46382b609c133da2b
Yan-P <yan.pishchan@noseryoung.com>
2023-06-27 16:09:23 +0200

    changed reservation class
    changed the fields of the reservation class

--- rest-foods/src/main/java/ch/noseryoung/blj/restfoods/domain/reservation/Reservation.java
+++ rest-foods/src/main/java/ch/noseryoung/blj/restfoods/domain/reservation/Reservation.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,20 +15,24 @@ import java.sql.Date;
 @Getter
 @Setter
 public class Reservation {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer reservationID;
 
-    @Positive(message = "reserver's ID must be positive")
-    @NotNull(message = "reserver's ID can't be null")
-    private Integer reserverID;
+    @NotNull(message = "The first name must be stated")
+    private String firstName;
 
-    @NotNull(message = "The date can't be null")
-    private Date dateAndTime;
+    @NotNull(message = "The last name must pe stated")
+    private String lastName;
 
-    @Positive(message = "table number Must be positive")
-    @NotNull(message = "table number can't be null")
-    private Integer tableNumber;
+    @NotNull(message = "Tne number of visitors must be specified")
+    private Integer visitorCount;
 
+    @NotNull(message = "The date must be specified")
+    private Date date;
+
+    @NotNull(message = "Time must be specified")
+    private String time;
+
+    private String phoneNumber;
 }
